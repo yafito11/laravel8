00631f7759d1364779448ec28820c9f3ee6606f9
yafito11 <yafiemember15@gmail.com>
2021-12-27 02:07:16 +0700

    menambahkan api json

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostApiController;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        "active" => 'home'
+    ]);
+});
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "active" => 'about',
+        "name" => "Muhammad Yafie Yulianto",
+        "email" => "yafieyulianto@dream.com",
+        "image" => "immfix.png"
+    ]);
+});
+
+
+Route::get('/posts', [PostApiController::class, 'index']);
+// halaman single post
+Route::get('posts/{post:slug}', [PostApiController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post categories',
+        'active' => 'categories',
+        'categories' => Category::all(),
+
+    ]);
 });
